b35b9d394e0785e30ccdcb5c669d80105ee10b90
AquiburRKhan <ark.aquib17@gmail.com>
2017-06-11 21:26:08 +0600

    change: change in api.js

--- app/config/api.js
+++ app/config/api.js
@@ -1,24 +1,26 @@
-const BASE_URL = 'https://www.rehive.com/api/3/'
+const BASE_URL = 'https://www.rehive.com/api/3/';
 
 const HEADERS = {
     'Content-Type': 'application/json'
-}
+};
 
 let _call = async (url, _method, data) => {
     try {
         let response = await fetch(BASE_URL + url, {
             method: _method,
             headers: HEADERS,
-            body: JSON.stringify(data)})
-        let responseJson = await response.json()
+            body: JSON.stringify(data)});
+        let responseJson = await response.json();
         return responseJson
     } catch (error) {
         console.log(error)
     }
-}
+};
 
-export default API = {
+const API = {
     login: (data) => {
         return _call('auth/login/', 'POST', data)
     }
-}
+};
+
+export default API;
